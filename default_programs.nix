87fcd1f084dccababc0e6314e726fc02f9ccb4ae
AfkaraLP <kijo@gmx.net>
2025-05-08 19:22:40 -0400

    some firefox config

--- default_programs.nix
+++ default_programs.nix
@@ -6,6 +6,30 @@
     firefox = {
       enable = true; 
       package = (pkgs.wrapFirefox.override { libpulseaudio = pkgs.libpressureaudio; }) pkgs.firefox-unwrapped { };
+      preferences = {
+        # "widget.use-xdg-desktop-portal.file-picker" = 1;
+      }; # preferences 
+      policies = {
+        PromptForDownloadLocation = "enabled";
+	Cookies = {
+	  Allow = [];
+	  AllowSessions = [];
+	  Block = [];
+	  Behavior = "reject-tracker-and-partition-foreign";
+	}; # Cookies 
+	DisablePocket = true;
+	DisableTelemetry = true;
+	DisableSetDesktopBackground = true;
+	EnableTrackingProtection = {
+	  Value = true;
+	  Cryptomining = true;
+	  Fingerprinting = true;
+	  EmailTracking = true;
+	}; # EnableTrackingProtection 
+	TranslateEnabled = false;
+	DontCheckDefaultBrowser = true;
+	SkipTermsOfUse = true;
+      }; # policies 
     }; # firefox 
   
     git.enable = true;
@@ -24,3 +48,4 @@
   }; # programs 
 
 }
+
