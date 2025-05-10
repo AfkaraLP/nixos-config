9043c8413e49f4c7624dbc3a3cc8bf50ad20c547
AfkaraLP <kijo@gmx.net>
2025-05-10 16:56:34 +0200

    eeeee

--- home-modules/waybar.nix
+++ home-modules/waybar.nix
@@ -28,12 +28,14 @@
 	  max-length = 110;
           exec = pkgs.writeShellScript "dadJoke" ''
             joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/)
+	    mkdir -p ./tmp
 	    touch ./tmp/waybar-dadjoke.txt
 	    echo "$joke" > ./tmp/waybar-dadjoke.txt
 	    echo "$joke"
           '';
 	  on-click-left = pkgs.writeShellScript "dadJokeReroll" ''
             joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/)
+	    mkdir -p ./tmp
 	    touch ./tmp/waybar-dadjoke.txt
 	    echo "$joke" > ./tmp/waybar-dadjoke.txt
 	    echo "$joke"
