43b1ecf7fe9b7d188d0a49799b7b9d9f7ae0d09a
AfkaraLP <kijo@gmx.net>
2025-05-13 20:38:57 +0200

    vim setup

--- nixvim_options/plugins.nix
+++ nixvim_options/plugins.nix
@@ -1,50 +1,61 @@
-{ config, pkgs, lib, nixvim, ... }: 
+{ config, pkgs, lib, nixvim, ... }:
 
 {
   programs.nixvim.plugins = {
-	  lazy.enable = true;
-	  guess-indent.enable = true;
-	  cmp-snippy.enable = true;
-        telescope.enable = true;
-        lualine.enable = true;
-        snacks.enable = true;
-        fzf-lua.enable = true;
-        which-key.enable = true;
-        mini = {
-          enable = true;
-          # mockDevIcons = true;
-        }; # mini
-        oil.enable = true;
-        blink-cmp.enable = true;
-        lazydev.enable = true;
-        bufferline.enable = true;
-        noice.enable = true;
-	transparent.enable = true;
-        trouble.enable = true;
-        conform-nvim = {
-	  enable = true;
-	  settings = {
-	    format_after_save.lsp_format = "first";
-	  };
-	}; # conform-nvim 
+    guess-indent.enable = true;
+    cmp-snippy.enable = true;
+    telescope.enable = true;
+    lualine.enable = true;
+    snacks.enable = true;
+    fzf-lua.enable = true;
+    which-key.enable = true;
+    mini = {
+      enable = true;
+      # mockDevIcons = true;
+    }; # mini
+    oil.enable = true;
+    blink-cmp.enable = true;
+    lazydev.enable = true;
+    cmp.enable = true;
+    bufferline.enable = true;
+    noice.enable = true;
+    efmls-configs = {
+      enable = true;
 
-        lsp = {
-          enable = true;
-	  inlayHints = true;
+      setup = {
+        all.linter = "alex";
+        nix = {
+          formatter = "alejandra";
+          linter = "statix";
+        }; # nix
+        rust = { formatter = "rustfmt"; };
+        lua.formatter = "stylua";
+        html.formatter = "prettier";
+        json.formatter = "prettier";
+      }; # setup
+    }; # efmls-configs
+    transparent.enable = true;
+    trouble.enable = true;
+    conform-nvim = {
+      enable = true;
+      settings = { format_after_save.lsp_format = "first"; };
+    }; # conform-nvim
 
-          servers = {
-            rust_analyzer = {
-              enable = true;
-            };
-            nixd.enable = true;
-            jsonls.enable = true;
-            tailwindcss.enable = true;
-            html.enable = true;
-            jedi_language_server.enable = true;
-            yamlls.enable = true;
-            quick_lint_js.enable = true;
-            bashls.enable = true;
-          }; # servers
-        }; # lsp};
-}; # nixvim.plugins 
+    lsp = {
+      enable = true;
+      inlayHints = true;
+
+      servers = {
+        rust_analyzer = { enable = true; };
+        nixd.enable = true;
+        jsonls.enable = true;
+        tailwindcss.enable = true;
+        html.enable = true;
+        jedi_language_server.enable = true;
+        yamlls.enable = true;
+        quick_lint_js.enable = true;
+        bashls.enable = true;
+      }; # servers
+    }; # lsp};
+  }; # nixvim.plugins
 }
