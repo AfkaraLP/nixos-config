77113dd5d127745d7d564351d6dd385141f43cf5
AfkaraLP <kijo@gmx.net>
2025-06-08 23:45:08 +0200

    zoxide + nushell

--- home-modules/terminal.nix
+++ home-modules/terminal.nix
@@ -2,33 +2,47 @@
 
 {
   programs = {
-      zoxide = {
-        enable = true;
-        enableNushellIntegration = true;
-      }; # zoxide
- 
-      wezterm = {
-        enable = true;
-        extraConfig = ''
-          local config = wezterm.config_builder()
-      	  enable_wayland = false
- 
-       	  config.font = wezterm.font 'Jetbrains Mono'
-          config.font_size = 12.0
-          config.hide_tab_bar_if_only_one_tab = true
-          config.window_background_opacity = 0.5
-          config.window_close_confirmation = "NeverPrompt"
-          config.window_padding = {
-            left = 2,
-            top = 0,
-            bottom = 2,
-            right = 2,
-          }
-          config.window_decorations = "NONE"
-          config.window_frame = { inactive_titlebar_bg = "#000000", }
- 
-          return config
-        '';
+
+    nushell = {
+      enable = true;
+
+      shellAliases = {
+        nrs = "sudo nixos-rebuild switch";
+        la = "ls -A";
+        ll = "ls -lA";
+        l = "ls";
+      };
+    };
+
+    zoxide = {
+      enable = true;
+      enableNushellIntegration = true;
+    }; # zoxide
+
+    wezterm = {
+      enable = true;
+      extraConfig = ''
+        local config = wezterm.config_builder()
+    	  enable_wayland = false
+
+        config.default_prog = { "nu" }
+
+     	  config.font = wezterm.font 'Jetbrains Mono'
+        config.font_size = 12.0
+        config.hide_tab_bar_if_only_one_tab = true
+        config.window_background_opacity = 0.5
+        config.window_close_confirmation = "NeverPrompt"
+        config.window_padding = {
+          left = 2,
+          top = 0,
+          bottom = 2,
+          right = 2,
+        }
+        config.window_decorations = "NONE"
+        config.window_frame = { inactive_titlebar_bg = "#000000", }
+
+        return config
+      '';
 
     }; # wezterm
   }; # programs 
