ed1e71cba8e9b7de3f6ab644132068949f5fb09c
AfkaraLP <kijo@gmx.net>
2025-06-10 20:50:18 +0200

    this time fix dad joke frfr no cap 100% free halal download no clickbait (gone sexual) (gone wrong) (the police was called)

--- home-modules/waybar.nix
+++ home-modules/waybar.nix
@@ -32,14 +32,16 @@
           interval = 10;
 	  max-length = 110;
           exec = pkgs.writeShellScript "dadJoke" ''
-            joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
+            # joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
+            joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | tr -d '\n' | tr -d '\r')
       	    mkdir -p ~/.tmp
       	    touch ~/.tmp/waybar-dadjoke.txt
       	    echo "$joke" > ~/.tmp/waybar-dadjoke.txt
       	    echo "$joke"
           '';
 	  on-click-left = pkgs.writeShellScript "dadJokeReroll" ''
-            joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
+            # joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
+            joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | tr -d '\n' | tr -d '\r')
       	    mkdir -p ~/.tmp
       	    touch ~/.tmp/waybar-dadjoke.txt
       	    echo "$joke" > ~/.tmp/waybar-dadjoke.txt
