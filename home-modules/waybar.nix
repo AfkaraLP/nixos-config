5223c7b5d876e86d4fcac5bb37d50f989cc9551c
AfkaraLP <kijo@gmx.net>
2025-07-02 23:45:38 +0200

    yeah waybar wooooo

--- home-modules/waybar.nix
+++ home-modules/waybar.nix
@@ -5,100 +5,112 @@
     enable = true;
     settings = {
       mainBar = {
-        output = [
-      	  "DP-1"
-      	  "DP-2"
-      	  "DP-3"
-      	];
+        output = [ "DP-1" "DP-2" "DP-3" ];
         layer = "top";
-      	position = "top";
-      	height = 35;
-      	margin = "10 5 0 5";
+        position = "top";
+        height = 35;
+        margin = "10 5 0 5";
 
-      	modules-left = [ "cpu" "memory" ];
-      	modules-center = [ "custom/dadJoke" ];
-      	modules-right = [ "wireplumber" "clock" ];
+        modules-left = [ "cpu" "memory" ];
+        modules-center = [ "custom/dadJoke" ];
+        modules-right = [ "wireplumber" "clock" ];
 
-      	"cpu" = {
-      	  format = "CPU: {}";
-      	}; # "cpu" 
+        cpu = {
+          interval = 1;
+          format = "cpu: {icon}{usage}%";
+          format-icons = [
+            "<span color='#69ff94'>▁</span>" 
+            "<span color='#2aa9ff'>▂</span>" 
+            "<span color='#f8f8f2'>▃</span>" 
+            "<span color='#f8f8f2'>▄</span>" 
+            "<span color='#ffffa5'>▅</span>" 
+            "<span color='#ffffa5'>▆</span>" 
+            "<span color='#ff9977'>▇</span>" 
+            "<span color='#dd532e'>█</span>"  
+          ];
+        }; # "cpu"
 
-      	"memory" = {
-	  
-      	};
+        clock = {
+          tooltip = true;
+          tooltip-format = "{:%Y-%m-%d}";
+        };
 
-	"custom/dadJoke" = {
+        memory = {
+          interval = 5;
+          format = "{used:0.1f}/{total:0.1f}GiB";
+        }; # "memory"
+
+        "custom/dadJoke" = {
           format = "My Dad Once Said: {}";
           interval = 10;
-	  max-length = 110;
+          max-length = 110;
           exec = pkgs.writeShellScript "dadJoke" ''
-            # joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
-            joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | tr -d '\n' | tr -d '\r')
-      	    mkdir -p ~/.tmp
-      	    touch ~/.tmp/waybar-dadjoke.txt
-      	    echo "$joke" > ~/.tmp/waybar-dadjoke.txt
-      	    echo "$joke"
+                  # joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
+                  joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | tr -d '\n' | tr -d '\r')
+            	    mkdir -p ~/.tmp
+            	    touch ~/.tmp/waybar-dadjoke.txt
+            	    echo "$joke" > ~/.tmp/waybar-dadjoke.txt
+            	    echo "$joke"
           '';
-	  on-click-left = pkgs.writeShellScript "dadJokeReroll" ''
-            # joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
-            joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | tr -d '\n' | tr -d '\r')
-      	    mkdir -p ~/.tmp
-      	    touch ~/.tmp/waybar-dadjoke.txt
-      	    echo "$joke" > ~/.tmp/waybar-dadjoke.txt
-      	    echo "$joke"
-      	  '';
-	  on-click-right = pkgs.writeShellScript "dadJokeCopy" ''
-      	    cat ~/.tmp/waybar-dadjoke.txt | wl-copy
-      	  '';
-        }; # "custom/dadJoke" 
-      }; # mainBar 
-    }; # settings 
+          on-click-left = pkgs.writeShellScript "dadJokeReroll" ''
+                  # joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | sed -z 's/[\n\r]+/, /g')
+                  joke=$(curl -H "Accept: text/plain" https://icanhazdadjoke.com/ | tr -d '\n' | tr -d '\r')
+            	    mkdir -p ~/.tmp
+            	    touch ~/.tmp/waybar-dadjoke.txt
+            	    echo "$joke" > ~/.tmp/waybar-dadjoke.txt
+            	    echo "$joke"
+            	  '';
+          on-click-right = pkgs.writeShellScript "dadJokeCopy"
+            "  cat ~/.tmp/waybar-dadjoke.txt | wl-copy\n";
+        }; # "custom/dadJoke"
+      }; # mainBar
+    }; # settings
     style = ''
-	      * {
-	  background-color: transparent;
-	  color: #FFF;
-	  margin: 0px;
-	  padding: 0px;
-	}
+            * {
+        background-color: transparent;
+        color: #FFF;
+        margin: 0px;
+        padding: 0px;
+      }
 
-	/* alone */
-	#custom-dadJoke {
-	  background-color: rgba(0,0,0,0.5);
-	  border: solid 2px rgba(58,58,58,0.66);
-	  border-radius: 10px 10px 10px 10px;
-	  margin: 0px 5px;
-	  padding: 10px;
-	}
+      /* alone */
+      #custom-dadJoke {
+        background-color: rgba(0,0,0,0.5);
+        border: solid 2px rgba(58,58,58,0.66);
+        border-radius: 10px 10px 10px 10px;
+        margin: 0px 5px;
+        padding: 10px;
+      }
 
-	/* left */
-	#cpu, #wireplumber {
-	  background-color: rgba(0,0,0,0.5);
-	  border: solid 2px rgba(58,58,58,0.66);
-	  border-radius: 10px 0px 0px 10px;
-	  margin: 0px 5px;
-	  padding: 10px;
-	}
+      /* left */
+      #cpu, #wireplumber {
+        background-color: rgba(0,0,0,0.5);
+        border: solid 2px rgba(58,58,58,0.66);
+        border-radius: 10px 0px 0px 10px;
+        margin: 0px 5px;
+        padding: 10px;
+      }
 
-	/* middle */
-	#middle {
-	  background-color: rgba(0,0,0,0.5);
-	  border: solid 2px rgba(58,58,58,0.66);
-	  border-radius: 0px 0px 0px 0px;
-	  margin: 0px 5px;
-	  padding: 10px;
-	}
+      /* middle */
+      #middle {
+        background-color: rgba(0,0,0,0.5);
+        border: solid 2px rgba(58,58,58,0.66);
+        border-radius: 0px 0px 0px 0px;
+        margin: 0px 5px;
+        padding: 10px;
+      }
 
-	/* right */
-	#memory, #clock {
-	  background-color: rgba(0,0,0,0.5);
-	  border: solid 2px rgba(58,58,58,0.66);
-	  border-radius: 0px 10px 10px 0px;
-	  margin: 0px 5px;
-	  padding: 10px;
-	}
+      /* right */
+      #memory, #clock {
+        background-color: rgba(0,0,0,0.5);
+        border: solid 2px rgba(58,58,58,0.66);
+        border-radius: 0px 10px 10px 0px;
+        margin: 0px 5px;
+        padding: 10px;
+      }
     '';
     # ENABLE THIS FOR DEBUG
     # systemd.enableInspect = true;
     # systemd.enable = true;
-  }; # programs.waybar 
+  }; # programs.waybar
 }
