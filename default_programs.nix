1f014f14b4c77f8cfb6230564149178fd8bf1a47
AfkaraLP <kijo@gmx.net>
2025-07-19 23:46:42 +0200

    steam unbreakingings

--- default_programs.nix
+++ default_programs.nix
@@ -22,9 +22,6 @@
       enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
     };
 
     firefox = {
