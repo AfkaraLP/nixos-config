95dd9a572c3d2461e8e93e2d4afc39b9b9f72e40
AfkaraLP <kijo@gmx.net>
2025-12-08 23:08:00 +0100

    new colourrrrrrsss

--- home-modules/terminal.nix
+++ home-modules/terminal.nix
@@ -39,6 +39,7 @@
 
             config.default_prog = { "nu" }
 
+            config.color_scheme = 'Catppuccin Mocha'
          	  config.font = wezterm.font 'Fira Code'
             config.font_size = 12.0
             config.hide_tab_bar_if_only_one_tab = true
