2b0eef865d106b548edbe514032f03888bdcbe12
AfkaraLP <kijo@gmx.net>
2026-03-01 19:23:25 +0100

    fix: improve boot time

--- .hidden/boot_options.nix
+++ .hidden/boot_options.nix
@@ -4,26 +4,24 @@
   ### BOOT
   systemd = {
     services = {
-      systemd-udev-settle.enable = false; # if all shit fails enable that again
+      systemd-udev-settle.enable = false; # enable if stuff fails
       NetworkManager-wait-online.enable = false;
       systemd-timesyncd.enable = false;
       systemd-rfkill.enable = false;
 
-      firewall = {
-        # after = [ "network-online.target" ];
-      	wantedBy = [ "multi-user.target" ];
-      }; # "firewall" 
+      firewall = { wantedBy = [ "multi-user.target" ]; };
 
       nscd.serviceConfig.TimeoutStopSec = "2s";
       NetworkManager-wait-online.serviceConfig.TimeoutStopSec = "2s";
       nscd.enable = false;
       disable-wifi-on-boot = {
         restartIfChanged = false;
-	after = [ "NetworkManager.service" ];
+        after = [ "NetworkManager.service" ];
       };
-    }; # services 
+    };
     tpm2.enable = false;
-  }; # systemd.services
+  };
+
   powerManagement = {
     enable = true;
     cpuFreqGovernor = "performance";
@@ -31,33 +29,31 @@
   services = {
     ntp.enable = false;
     chrony.enable = true;
-  }; # services
-  # systemd.extraConfig = ''
-  #   DefaultTimeoutStopSec=10s
-  # '';
-  ### BOOT
+  };
 
-    boot = {
+  # Boot optimizations
+  boot = {
     loader = {
       systemd-boot.configurationLimit = 3;
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
-    }; # loader
+    };
 
-    kernelParams = [ "rootflags=ro" "fsck.mode=skip" "amd_pstate=active"];
+    kernelParams =
+      [ "rootflags=ro" "fsck.mode=skip" "amd_pstate=active" "quiet" ];
 
     initrd = {
       compressor = "zstd";
       availableKernelModules = [ "nvme" "ext4" ];
-    }; # initrd
-  }; # boot
+    };
+  };
 
   boot.binfmt.registrations.appimage = {
     wrapInterpreterInShell = false;
     interpreter = "${pkgs.appimage-run}/bin/appimage-run";
     recognitionType = "magic";
     offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
-  }; # boot.binfmt.registrations.appimage 
+    mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+    magicOrExtension = "\\x7fELF....AI\\x02";
+  };
 }
